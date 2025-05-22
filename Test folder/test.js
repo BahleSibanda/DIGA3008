bf0bca344428f1f5898521c55bd4eefaf5ffcd01
BahleSibanda <2716324@students.wits.ac.za>
2025-05-23 00:23:14 +0200

    Javascript 1

--- Test folder/test.js
+++ Test folder/test.js
@@ -1,1 +1,14 @@
-console.log("TESTING THE JS")
+function calculateRectangleArea()
+   {return length*width;}
+
+    let width=5;
+    let length=6;
+
+    let area = width* length;
+
+    function calculateSum(int1, int2)
+    { let sum = int1 + int2}
+if (int1==int2)
+{return int * 3}
+
+
